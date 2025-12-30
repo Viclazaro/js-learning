1b28698e6e623cf1c33728af8721d7cb7a409c26
Yen <thearyazitah@gmail.com>
2025-12-30 10:10:35 -0300

    resolução de exercicios

--- starter-challenges/challenges-11-16.js
+++ starter-challenges/challenges-11-16.js
@@ -1,13 +1,25 @@
-//Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+//11)Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
 
 let resposta = prompt('Qual é o dia da semana?');
 
 if(resposta == domingo){
-    alert('Bom fim de semana!')
+    alert('Bom fim de semana!');
 
     elseif(resposta == sábado)
-        alert('Bom fim de semana!')
+        alert('Bom fim de semana!');
 } else{
-    alert('Boa semana!')
+    alert('Boa semana!');
 
 }
+
+//12)Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+let numero = prompt('Digite um numero: ');
+if(numero > 0){
+    alert('numero positivo');
+
+}else{
+    alert('numero negativo');
+
+}
+
+    

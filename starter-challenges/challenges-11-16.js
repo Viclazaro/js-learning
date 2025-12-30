ea9c24e5def422b399a2b38f871370432a2cf3b9
Yen <thearyazitah@gmail.com>
2025-12-30 10:04:07 -0300

    resolução exercicio 1

---
+++ starter-challenges/challenges-11-16.js
@@ -0,0 +1,13 @@
+//Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+
+let resposta = prompt('Qual é o dia da semana?');
+
+if(resposta == domingo){
+    alert('Bom fim de semana!')
+
+    elseif(resposta == sábado)
+        alert('Bom fim de semana!')
+} else{
+    alert('Boa semana!')
+
+}
